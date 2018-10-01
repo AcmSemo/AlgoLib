6e1ebca0ced21759c5b6d02c62002a1c457fb87c
Joey <me@joeykanaly.com>
2018-10-01 10:41:42 -0500

    Removed unnecessary code

--- CSharp/BubbleSort.cs
+++ CSharp/BubbleSort.cs
@@ -1,32 +1,16 @@
-namespace Program
+public static int[] BubbleSort(int[] listOfNumbers)
 {
-    public class Sort
+    for (int i = 0; i < listOfNumbers.Length - 1; i++)
     {
-        public static int[] BubbleSort(int[] listOfNumbers)
+        for (int j = 0; j < listOfNumbers.Length - i - 1; j++)
         {
-            for (int i = 0; i < listOfNumbers.Length - 1; i++)
+            if (listOfNumbers[j] > listOfNumbers[j + 1])
             {
-                for (int j = 0; j < listOfNumbers.Length - i - 1; j++)
-                {
-                    if (listOfNumbers[j] > listOfNumbers[j + 1])
-                    {
-                        int temp = listOfNumbers[j + 1];
-                        listOfNumbers[j + 1] = listOfNumbers[j];
-                        listOfNumbers[j] = temp;
-                    }
-                }
-            }
-            return listOfNumbers;
-        }
-
-        public static void Main(string[] Args)
-        {
-            int[] list = { 7, 4, 6, 10, 8, 3, 5, 9, 2, 1 };
-            BubbleSort(list);
-            foreach (var item in list)
-            {
-                System.Console.Out.WriteLine(item + ", ");
+                int temp = listOfNumbers[j + 1];
+                listOfNumbers[j + 1] = listOfNumbers[j];
+                listOfNumbers[j] = temp;
             }
         }
     }
+    return listOfNumbers;
 }
