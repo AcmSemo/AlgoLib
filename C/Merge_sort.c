cd6c34abe07fa74ad16bae76b161502c1be4ffe4
AdarshKC <33431801+AdarshKC@users.noreply.github.com>
2018-10-01 23:17:35 +0530

    Create Merge_sort.c

---
+++ C/Merge_sort.c
@@ -0,0 +1,91 @@
+// C code to sort array by merge agorithm...
+
+#include<stdlib.h> 
+#include<stdio.h> 
+  
+// Function to merge...
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    int L[n1], R[n2]; 
+  
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+// Function to sort...
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        int m = l+(r-l)/2; 
+  
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+  
+// Function to print an array...
+void print(int A[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+} 
+
+// MAIN FUNCTION...
+int main()
+{
+	int n,i;
+	printf("Enter the number of elements of your array : ");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the %d elements one by one:-\n",n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	printf("\nInput: ");
+	print(a,n);
+	mergeSort(a, 0, n - 1);	
+	printf("\nOutput: ");
+	print(a,n);
+}
