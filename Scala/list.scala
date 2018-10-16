e4b06bc2f09c4feb0a18ca58400f14bb9c4bbb61
Stephen Sladek <scsladek91@gmail.com>
2018-10-15 21:28:54 -0500

    Add comments
    Moved your comments about the methods from description into the code file itself

--- Scala/list.scala
+++ Scala/list.scala
@@ -6,19 +6,22 @@ object List {
   case class Nil[T]()                              extends LinkedList[T]
   case class Node[T](data: T, next: LinkedList[T]) extends LinkedList[T]
 
-
+  // Adds an item to the beginning of the list
   def prepend[T](data: T, next: LinkedList[T]): LinkedList[T] = Node(data, next)
 
+  // Gets all of the list except for the first element
   def rest[T](lst: LinkedList[T]): Option[LinkedList[T]] = lst match {
     case Nil()          => None
     case Node(_, rest) => Some(rest)
   }
 
+  // Gets the first item of the list (if any)
   def first[T](lst: LinkedList[T]): Option[T] = lst match {
     case Nil()          => None
     case Node(first, _) => Some(first)
   }
 
+  // Gets the number of items in the list
   def length[T](lst: LinkedList[T]): Int = {
     @tailrec
     def inner(lst: LinkedList[T], acc: Int): Int = lst match {
@@ -29,6 +32,7 @@ object List {
     inner(lst, 0)
   }
 
+  // Returns a reverse version of the list
   def reverse[T](lst: LinkedList[T]): LinkedList[T] = {
     @scala.annotation.tailrec
     def inner(lst: LinkedList[T], acc: LinkedList[T]): LinkedList[T] = lst match {
@@ -39,6 +43,7 @@ object List {
     inner(lst, Nil())
   }
 
+  // Returns the index of the item being searched for if it is in the list
   def find[T](item: T, lst: LinkedList[T]): Option[Int] = {
     @scala.annotation.tailrec
     def inner(lst: LinkedList[T], acc: Int): Option[Int] = lst match {
@@ -50,12 +55,14 @@ object List {
     inner(lst, 0)
   }
 
+  // Returns the last element of the list
   def last[T](lst: LinkedList[T]): Option[T] = lst match {
     case Nil()          => None
     case Node(x, Nil()) => Some(x)
     case Node(x, xs)    => last(xs)
   }
 
+  // Returns a list from a bunch of parameters (list(1,2,3,4,5))
   def list[T](items: T*): LinkedList[T] = {
     var lst: LinkedList[T] = Nil()
 
@@ -66,6 +73,7 @@ object List {
     reverse(lst)
   }
 
+  // Returns a list containing only the members of the original list which passed a test (the filter)
   def filter[T](lst: LinkedList[T], fn: T => Boolean): LinkedList[T] = {
     @scala.annotation.tailrec
     def inner(lst: LinkedList[T], acc: LinkedList[T]): LinkedList[T] = lst match {
@@ -79,7 +87,7 @@ object List {
     inner(lst, Nil())
   }
 
-
+  // Returns a list containing all of the elements of the original list with a transform function applied to each element
   def map[T, U](lst: LinkedList[T], fn: T => U): LinkedList[U] = {
     @scala.annotation.tailrec
     def inner(lst: LinkedList[T], acc: LinkedList[U]): LinkedList[U] = lst match {
